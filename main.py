d13a22e3c0d488b99ab2060a1e13cd09f86c3565
Yevhen Liubarskyi <33.heavens@gmail.com>
2022-12-18 22:55:50 +0200

    Coded the delete method.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -34,6 +34,18 @@ posts = [
 ]
 
 
+def find_post(id):
+    for p in posts:
+        if p["author"]["id"] == id:
+            return p
+
+
+def find_post_index(id):
+    for i, p in enumerate(posts):
+        if p["author"]["id"] == id:
+            return i
+
+
 @app.get("/")
 def home():
     return {"Data": "Welcome"}
@@ -46,13 +58,19 @@ def get_all_posts():
 
 @app.get("/posts/{id}")
 def get_a_post_detail(id: int):
-    return posts[id - 1]
+    post = find_post(id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The post with id {id} was not found.",
+        )
+    return {"post_detail": post}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_a_new_post(post: Post):
     post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
+    post_dict["id"] = randrange(0, 1000000)
     posts.append(post_dict)
     return post_dict
 
@@ -62,6 +80,13 @@ def edit_a_post(id: int, post: Post):
     pass
 
 
-@app.delete("/posts/{id}")
-def edit_a_post(id: int):
-    pass
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_a_post(id: int):
+    index = find_post_index(id)
+    if not index:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The post with id {id} does not exist.",
+        )
+    posts.pop(index)
+    # return Response(status_code=status.HTTP_204_NO_CONTENT)
