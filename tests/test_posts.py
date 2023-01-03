e5698642e06b1633413d7ba8c13abd459f683576
Yevhen Liubarskyi <33.heavens@gmail.com>
2023-01-03 16:25:33 +0200

    Added create post tests.

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app import schemas
 
 
@@ -41,3 +43,53 @@ def test_post_detail_authorized_author(authorized_client, test_posts):
 
     assert res.status_code == 200
     assert post.title == test_posts[0].title
+
+
+@pytest.mark.parametrize(
+    "title, description, price",
+    [
+        ("awesome new title", "awesome new description", 33333),
+        ("favorite pizza", "I love peperoni", 22222),
+        ("awesome weather today", "awesome blues skies", 55555),
+    ],
+)
+def test_create_post(
+    authorized_client, test_author, test_posts, title, description, price
+):
+    res = authorized_client.post(
+        "/posts",
+        json={
+            "title": title,
+            "description": description,
+            "price": price,
+        },
+    )
+
+    created_post = schemas.PostResponse(**res.json())
+
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.description == description
+    assert created_post.price == price
+    assert created_post.author.id == test_author["id"]
+
+
+@pytest.mark.parametrize(
+    "title, description, price",
+    [
+        ("awesome new title", "awesome new description", 33333),
+        ("favorite pizza", "I love peperoni", 22222),
+        ("awesome weather today", "awesome blues skies", 55555),
+    ],
+)
+def test_create_post_unauthorized(client, test_posts, title, description, price):
+    res = client.post(
+        "/posts",
+        json={
+            "title": title,
+            "description": description,
+            "price": price,
+        },
+    )
+
+    assert res.status_code == 401
